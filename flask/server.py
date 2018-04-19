4b3123487ae419cd38b3ab2c837677d57dbc555a
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-04-19 15:08:20 +1000

    added example post route

--- flask/server.py
+++ flask/server.py
@@ -8,4 +8,10 @@ def index():
     return "Hello, World!"
 
 
+@app.route('/answer', methods=['POST'])
+def handle_answer():
+    req_data = request.get_json()
+
+    answer_base64 = req_data['answer']
+
 app.run()
